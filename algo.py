e7d5bed6b291bbc312b5b0c0e5f9a14284052c70
Daniel <danielrferriss@gmail.com>
2017-10-16 17:08:26 -0500

    This is an absolute disaster.

---
+++ algo.py
@@ -0,0 +1,10 @@
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ts = TimeSeries(key='XP12DSLMVA2QP4MO', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='V',interval='15min', outputsize='full')
+data['close'].plot()
+print(data.describe())
+plt.title('Visa stock data stuff')
+plt.show()
