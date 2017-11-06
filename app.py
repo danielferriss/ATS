5e4b7be6ad8b554c399411cbd3623225e5f55755
Daniel <danielrferriss@gmail.com>
2017-11-06 16:03:57 -0600

    Dropdown menus

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'reallyreallyreallyreallysecretkey'
+print('I will kill you Danny -Ann')
+
 
 manager   = Manager(app)
 bootstrap = Bootstrap(app)
 moment    = Moment(app)
 
 class TickerForm(FlaskForm):
-    ticker1   = StringField('Ticker 1:',     validators=[Required()])
-    ticker2   = StringField('Ticker 2:',     validators=[Required()])
-    interval  = StringField('Time Interval:', validators=[Required()])
-    length    = StringField('Time Length:',  validators=[Required()])
-    submit    = SubmitField('Submit')
+    ticker1   = StringField(u'Ticker 1:',     validators=[Required()])
+    ticker2   = StringField(u'Ticker 2:',     validators=[Required()])
+    length    = SelectField(u'Time Length:', choices=[('1day', '1 day'), ('1week', '1 week'), ('2week', '2 weeks'), ('10week', '10 weeks'), ('1year', '1 year')])
+    interval  = SelectField(u'Time Interval:', choices=[('1', '1 second'), ('1 minute', '1 minute'), ('text', '1 hour'), ('text', '1 day')])
+    submit    = SubmitField(u'Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -35,3 +37,4 @@ def index():
     return render_template('index.html', form=form, ticker1=ticker1, ticker2=ticker2, interval=interval, length=length)
 
 if __name__ == '__main__':
+    app.run()
