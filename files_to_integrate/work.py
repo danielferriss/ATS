9c05f0cd1ee1e7b41bc83b07ddf44cfed77e1a33
dbhati2 <32111464+dbhati2@users.noreply.github.com>
2017-11-11 13:36:43 -0600

    Updated

--- files_to_integrate/work.py
+++ files_to_integrate/work.py
@@ -1,14 +1,29 @@
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
+
+ticker1 = 'msft'
+ticker2 = 'googl'
+interval = '15min'
+length = '1day'
 
 def stockchart(symbol1, symbol2):
     ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=symbol1,interval='15min', outputsize='compact')
     data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
-    data['close'].plot()
-    data2['close'].plot()
-    plt.title('Testing testing 123')
-    plt.show()
+    fig, ax1 = plt.subplots()
+    ax1.plot(data, 'b')
+    ax1.set_xlabel('Time(s)')
+    ax1.set_ylabel(symbol1.upper(), color = 'b')
 
-stockchart('msft','googl')
+    ax2 = ax1.twinx()
+    ax2.plot(data2, 'g')
+    ax2.set_ylabel(symbol2.upper(), color = 'g')
+    plt.title('Stock Graph of ' + symbol1.upper() + ' and ' + symbol2.upper())
+
+    plt.show()
+    print(data.describe())
+
+stockchart(ticker1,ticker2)
+
