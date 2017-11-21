36048993f3a5d3a6413d4b6399ef89aeac58fd81
danielferriss <31582914+danielferriss@users.noreply.github.com>
2017-11-21 02:44:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -18,27 +19,25 @@ bootstrap = Bootstrap(app)
 moment    = Moment(app)
 
 class TickerForm(FlaskForm):
-    ticker1   = StringField(u'Ticker 1:',     validators=[Required()])
-    ticker2   = StringField(u'Ticker 2:',     validators=[Required()])
+    ticker1   = StringField(u'Ticker 1:', validators=[Required()])
+    ticker2   = StringField(u'Ticker 2:', validators=[Required()])
     length    = SelectField(u'Time Length:', choices=[('', ''),('1day', '1 day'), ('1week', '1 week'), ('2week', '2 weeks'), ('10week', '10 weeks'), ('1year', '1 year')])
     submit    = SubmitField(u'Submit')
-
-nav.Bar('top', [
-    nav.Item('Home', 'index'),
-])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     ticker1  = None
     ticker2  = None
     length   = None
-    form = TickerForm()
+    graph    = None
+    form     = TickerForm()
     if form.validate_on_submit():
         ticker1  = form.ticker1.data
         ticker2  = form.ticker2.data
         length   = form.length.data
-        overall_graph(ticker1, ticker2, length, interval)
-    return render_template('index.html', form=form, ticker1=ticker1, ticker2=ticker2, length=length)
+        graph    = stockchart(ticker1, ticker2)
+        return render_template('index.html', form=form, ticker1=ticker1, ticker2=ticker2, length=length, graph = graph)
+    return render_template('index.html', form=form, ticker1=ticker1, ticker2=ticker2, length=length, graph = graph)
 
 @app.route('/team', methods=['GET', 'POST'])
 def team():
