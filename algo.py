bc56f9ec7dedb9cc7bb8780fd9bfd2e3e6a7e224
danielferriss <31582914+danielferriss@users.noreply.github.com>
2017-11-21 02:45:06 -0600

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,30 +1,35 @@
-# from alpha_vantage.timeseries import TimeSeries
-# import matplotlib.pyplot as plt
-# import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import sys
+import base64
+import numpy as np
+import io
+from flask import make_response, send_file
 
+ticker1 = 'msft'
+ticker2 = 'googl'
+interval = '15min'
+length = '1day'
 
-# def overall_graph(ticker1, ticker2, length, interval):
-#     pass
-# def zoomed_graph(ticker1, ticker2, length, interval):
-#     pass
-# def find_correlation(ticker1, ticker2, length, interval):
-#     t1cost = ts.get_intraday(symbol=ticker1,interval=interval, outputsize='full')
-#     t2cost = ts.get_intraday(symbol=ticker2,interval=interval, outputsize='full')
-#     return (t1cost)
+def stockchart(symbol1, symbol2):
+    ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=symbol1,interval='15min', outputsize='compact')
+    data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
+    fig, ax1 = plt.subplots()
+    ax1.plot(data, 'b')
+    ax1.set_xlabel('Time(s)')
+    ax1.set_ylabel(symbol1.upper(), color = 'b')
 
-# ticker1 = 'MSFT'
-# ticker2 = 'GOOGL'
-# length = 0
-# interval = '15min'
-# print find_correlation(ticker1, ticker2, length, interval)
+    ax2 = ax1.twinx()
+    ax2.plot(data2, 'g')
+    ax2.set_ylabel(symbol2.upper(), color = 'g')
+    plt.title('Stock Graph of ' + symbol1.upper() + ' and ' + symbol2.upper())
 
-# from alpha_vantage.timeseries import TimeSeries
-# import matplotlib.pyplot as plt
-# import pandas as pd
-
-# ts = TimeSeries(key='XP12DSLMVA2QP4MO', output_format='pandas')
-# data, meta_data = ts.get_intraday(symbol='V',interval='15min', outputsize='compact')
-# data['close'].plot()
-# print(data.describe())
-# plt.title('Visa stock data stuff')
-# plt.show()
+    canvas = FigureCanvas(fig)
+    output = io.BytesIO()
+    canvas.print_png(output)
+    # response = output.getvalue()
+    response = base64.b64encode(output.getvalue()).decode('ascii')
+    return response
