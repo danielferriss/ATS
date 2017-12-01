8547566323621cf7b35f1335274cd8d181d816ec
Daniel <danielrferriss@gmail.com>
2017-12-01 00:02:56 -0600

    cleaning up choices. added logo

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
+#           _____                _____                     _____          
+#          /\    \              /\    \                    /\    \         
+#         /::\    \            /::\    \                  /::\    \        
+#        /::::\    \           \:::\    \                /::::\    \       
+#       /::::::\    \           \:::\    \              /::::::\    \      
+#      /:::/\:::\    \           \:::\    \            /:::/\:::\    \     
+#     /:::/__\:::\    \           \:::\    \          /:::/__\:::\    \    
+#    /::::\   \:::\    \          /::::\    \         \:::\   \:::\    \   
+#   /::::::\   \:::\    \        /::::::\    \      ___\:::\   \:::\    \  
+#  /:::/\:::\   \:::\    \      /:::/\:::\    \    /\   \:::\   \:::\    \ 
+# /:::/  \:::\   \:::\____\    /:::/  \:::\____\  /::\   \:::\   \:::\____\
+# \::/    \:::\  /:::/    /   /:::/    \::/    /  \:::\   \:::\   \::/    /
+#  \/____/ \:::\/:::/    /   /:::/    / \/____/    \:::\   \:::\   \/____/ 
+#           \::::::/    /   /:::/    /              \:::\   \:::\    \     
+#            \::::/    /   /:::/    /                \:::\   \:::\____\    
+#            /:::/    /    \::/    /                  \:::\  /:::/    /    
+#           /:::/    /      \/____/                    \:::\/:::/    /     
+#          /:::/    /                                   \::::::/    /      
+#         /:::/    /                                     \::::/    /       
+#         \::/    /                                       \::/    /        
+#          \/____/                                         \/____/         
+#                                                                
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -6,7 +28,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
 from flask_navigation import Navigation
-from algo import *
+from algo import *       
+
 
 app = Flask(__name__)
 nav = Navigation(app)
@@ -16,11 +39,12 @@ app.config['SECRET_KEY'] = 'reallyreallyreallyreallysecretkey'
 manager   = Manager(app)
 bootstrap = Bootstrap(app)
 moment    = Moment(app)
+choices   = [('', ''),('1day', '1 day'), ('1week', '1 week'), ('4week', '4 weeks'), ('3month', '3 months'), ('1year', '1 year'), ('5year', '5 years')]
 
 class TickerForm(FlaskForm):
-    ticker1   = StringField(u'Ticker 1:', validators=[Required()])
-    ticker2   = StringField(u'Ticker 2:', validators=[Required()])
-    length    = SelectField(u'Time Length:', choices=[('', ''),('1day', '1 day'), ('1week', '1 week'), ('4week', '4 weeks'), ('3month', '3 months'), ('1year', '1 year'), ('5year', '5 years')])
+    ticker1   = StringField(u'Ticker 1:',    validators=[Required()])
+    ticker2   = StringField(u'Ticker 2:',    validators=[Required()])
+    length    = SelectField(u'Time Length:', validators=[Required()], choices=choices)
     submit    = SubmitField(u'Submit')
 
 @app.route('/', methods=['GET', 'POST'])
