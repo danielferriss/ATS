625368146bc174e7d9025f3512145b54e255a25f
Daniel <danielrferriss@gmail.com>
2017-11-30 22:53:38 -0600

    Graph return now works

--- algo.py
+++ algo.py
@@ -10,26 +10,28 @@ from flask import make_response, send_file
 
 ################ Directs data to correct function based on length ################
 def stockchart(symbol1, symbol2, length):
-    if length is '1day':
+    if length == '1day':
         return stockchart_1day(symbol1, symbol2)
 
-    if length is '1week':
+    if length == '1week':
         return stockchart_1weekday(symbol1, symbol2)
 
-    if length is '4week':
+    if length == '4week':
         return stockchart_4week(symbol1, symbol2)
 
-    if length is '3month':
+    if length == '3month':
         return stockchart_3month(symbol1, symbol2)
 
-    if length is '1year':
+    if length == '1year':
         return stockchart_1year(symbol1, symbol2)
 
-    if length is '5year':
+    if length == '5year':
         return stockchart_5year(symbol1, symbol2)
+    else:
+        return stockchart_1day(symbol1, symbol2)
 
 ############### Graphs inputted data and encodes to return to site ################
-def graph(symbol1, symbol2, data, data2, meta_data):
+def graph(symbol1, symbol2, data, data2, meta_data, ts):
     fig, ax1 = plt.subplots()
     ax1.plot(data, 'b')
     ax1.set_xlabel('Time(s)')
@@ -55,7 +57,7 @@ def stockchart_1day(symbol1, symbol2):
     ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=symbol1,interval='15min', outputsize='compact')
     data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
-    return graph(symbol1, symbol2, data, data2, meta_data)
+    return graph(symbol1, symbol2, data, data2, meta_data, ts)
     
 ################ 1 week function ################
 def stockchart_1week(symbol1, symbol2):
