05d47a238284c2f329861522b7f75b1272cdc3d7
Daniel <danielrferriss@gmail.com>
2017-11-30 22:17:01 -0600

    Made a list of symbols

---
+++ files_to_integrate/readcolumn/read.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import csv
+nasdaq = pd.read_csv("nasdaq.csv")
+nyse   = pd.read_csv("nyse.csv")
+amex   = pd.read_csv("amex.csv")
+
+nasdaq.columns = ["Symbol", "Name", "LastSale", "MarketCap", "IPOyear", "Sector", "industry", "Summary Quote", "None"]
+nyse.columns   = ["Symbol", "Name", "LastSale", "MarketCap", "IPOyear", "Sector", "industry", "Summary Quote", "None"]
+amex.columns   = ["Symbol", "Name", "LastSale", "MarketCap", "IPOyear", "Sector", "industry", "Summary Quote", "None"]
+
+nasdaq_symbols = list(nasdaq.Symbol)
+nyse_symbols   = list(nyse.Symbol)
+amex_symbols   = list(amex.Symbol)
+
+strip_nasdaq_symbols = [elem.strip() for elem in nasdaq_symbols]
+strip_nyse_symbols   = [elem.strip() for elem in nyse_symbols]
+strip_amex_symbols   = [elem.strip() for elem in amex_symbols]
+
+symbols = set().union(strip_nasdaq_symbols, strip_nyse_symbols, strip_amex_symbols)
+
+with open('symbols.csv', 'w') as f:
+    writer = csv.writer(f)
+    for val in symbols:
+        writer.writerow([val])
+
