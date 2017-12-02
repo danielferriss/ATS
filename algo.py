7fb0ae8babea4b5553b9e8e739cad25a2bd5c801
dbhati2 <32111464+dbhati2@users.noreply.github.com>
2017-12-02 16:12:24 -0600

    Edward is shook

--- algo.py
+++ algo.py
@@ -62,8 +62,6 @@ def stockchart_1day(symbol1, symbol2):
 	data2, meta_data = ts.get_intraday(symbol=symbol2,interval='60min', outputsize='compact')
 	newdata1 = data1.drop(data1.index[0:59])
 	newdata2 = data2.drop(data2.index[0:59])
-	print (newdata1)
-	print (newdata2)
 	return graph(symbol1, symbol2, newdata1, newdata2, meta_data, ts, '24 Hours')
 	
 ################ 1 week function ################
@@ -71,36 +69,47 @@ def stockchart_1week(symbol1, symbol2):
 	# Make the interval be hourly
 	title = '1 Week'
 	ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
-	data, meta_data = ts.get_weekly(symbol=symbol1)
-	data2, meta_data = ts.get_weekly(symbol=symbol2)
-	return graph(symbol1, symbol2, data, data2, meta_data, ts)
+	data1, meta_data = ts.get_daily(symbol=symbol1, outputsize='compact')
+	data2, meta_data = ts.get_daily(symbol=symbol2, outputsize='compact')
+	newdata1 = data1.drop(data1.index[0:93])
+	newdata2 = data2.drop(data2.index[0:93])
+	return graph(symbol1, symbol2, newdata1, newdata2, meta_data, ts, title)
 
 ################ 4 week function ################
 def stockchart_4week(symbol1, symbol2):
 	# Make the interval be hourly
 	title = '4 Weeks'
 	ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
-	data, meta_data = ts.get_monthly(symbol=symbol1)
-	data2, meta_data = ts.get_monthly(symbol=symbol2)
-	return graph(symbol1, symbol2, data, data2, meta_data, ts)
+	data1, meta_data = ts.get_daily(symbol=symbol1, outputsize='compact')
+	data2, meta_data = ts.get_daily(symbol=symbol2, outputsize='compact')
+	newdata1 = data1.drop(data1.index[0:78])
+	newdata2 = data2.drop(data2.index[0:78])
+	return graph(symbol1, symbol2, newdata1, newdata2, meta_data, ts, title)
 
 ################ 3 month function ################
 def stockchart_3month(symbol1, symbol2):
 	# Make the interval be daily
 	title = '3 Months'
 	ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
-	data, meta_data = ts.get_intraday(symbol=symbol1,interval='15min', outputsize='compact')
-	data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
-	return graph(symbol1, symbol2, data, data2, meta_data, ts)
+	data1, meta_data = ts.get_daily(symbol=symbol1, outputsize='compact')
+	data2, meta_data = ts.get_daily(symbol=symbol2, outputsize='compact')
+	newdata1 = data1.drop(data1.index[0:36])
+	newdata2 = data2.drop(data2.index[0:36])
+	print(newdata1)
+	return graph(symbol1, symbol2, newdata1, newdata2, meta_data, ts, title)
 
 ################ 1 year function ################
 def stockchart_1year(symbol1, symbol2):
 	# Make the interval be daily
 	title = '1 Year'
 	ts = TimeSeries(key='QBGZM8IV1P2X2VJQ', output_format='pandas')
-	data, meta_data = ts.get_intraday(symbol=symbol1,interval='15min', outputsize='compact')
-	data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
-	return graph(symbol1, symbol2, data, data2, meta_data, ts)
+	data1, meta_data = ts.get_weekly(symbol=symbol1)
+	data2, meta_data = ts.get_weekly(symbol=symbol2)
+	newdata1 = data1.drop(data1.index[0:len(data1.index) - 53])
+	newdata2 = data2.drop(data2.index[0:len(data2.index) - 53])
+	print(newdata1)
+	print(newdata2)
+	return graph(symbol1, symbol2, newdata1, newdata2, meta_data, ts, title)
 
 ################ 5 year function ################
 def stockchart_5year(symbol1, symbol2):
@@ -111,4 +120,4 @@ def stockchart_5year(symbol1, symbol2):
 	data2, meta_data = ts.get_intraday(symbol=symbol2,interval='15min', outputsize='compact')
 	return graph(symbol1, symbol2, data, data2, meta_data, ts)
 
-stockchart_1day('GOOGL','AAPL')
+stockchart_3month('GOOGL','AAPL')
