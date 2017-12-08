c64ec47f5c21a5f59b9a4544cc706f45f242c48a
ANiggy <anmoln2@illinois.edu>
2017-12-08 17:30:33 -0600

    this is mostly doneish

--- files_to_integrate/DetermineCorrelation.py
+++ files_to_integrate/DetermineCorrelation.py
@@ -6,6 +6,8 @@ import os
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+from pydoc import help
+from scipy.stats.stats import pearsonr
 
 
 ticker1 = "VG"
@@ -15,32 +17,16 @@ ts = TimeSeries(key='XP12DSLMVA2QP4MO', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol = ticker1,interval=time_interval, outputsize='full')
 stock2, meta_data = ts.get_intraday(symbol= ticker2,interval=time_interval, outputsize='full')
 
-#filter out values before 10am because they mess up the data
-index_value = data.index
-hours = index_value.str[11:13]
-minutes = index_value.str[14:16]
-df.ix[(hours == "09") & (30 <= minutes < 60)]
-
-# a = ts.keys()
-
-#filter out values before 10am because they mess up the data
-index_value = data.index
-hours = int(index_value.str[11:13])
-minutes = int(index_value.str[14:16])
-data.ix[(hours == "09") & (30 <= minutes < 60)]
-
-numpyStock1 = data['close'].as_matrix()
-numpyStock2 = stock2['close'].as_matrix()
 
 def fix_data(stock1, stock2):
     stock1.drop(['high'],   axis=1, inplace=True)
     stock1.drop(['low'],    axis=1, inplace=True)
-    stock1.drop(['close'],  axis=1, inplace=True)
+    stock1.drop(['open'],  axis=1, inplace=True)
     stock1.drop(['volume'], axis=1, inplace=True)
 
     stock2.drop(['high'],   axis=1, inplace=True)
     stock2.drop(['low'],    axis=1, inplace=True)
-    stock2.drop(['close'],  axis=1, inplace=True)
+    stock2.drop(['open'],  axis=1, inplace=True)
     stock2.drop(['volume'], axis=1, inplace=True)
 
 
@@ -66,30 +52,25 @@ def convert_to_slopes(arr_vals):
     next = 0
     size = len(numpyStock1)
     for index in range(1, len(arr_vals) - 2, 1):
-        prev = arr_vals[index - 1, 3]
-        next = arr_vals[index + 1, 3]
+        prev = arr_vals[index - 1]
+        next = arr_vals[index + 1]
         slope = next - prev
         if slope > 0:
             slopes.append(1)
         else:
-            slopes.append(0)
+            slopes.append(-1)
     return slopes
 
 
 #cost function
 def calc_cost(stock1, stock2):
-    cost = sp.chisquare(stock1, stock2)
+    cost = np.corrcoef(stock1, stock2)[0,1]
     return cost
 
 
     
 
-slopes1 = np.array(convert_to_slopes(numpyStock1))
-slopes2 = np.array(convert_to_slopes(numpyStock2))
 
-cost = calc_cost(slopes1, slopes2)
-#gets the first 5 vals
-data.head()
 
 #minimize cost (finding optimial time shift)
 def optimal_time_shift():
@@ -99,14 +80,41 @@ def optimal_time_shift():
     
 
 
+tuplea = fix_data(data, stock2)
+#filter out values before 10am because they mess up the data
+# index_value = data.index
+# hours = index_value.str[11:13]
+# minutes = index_value.str[14:16]
+# df.ix[(hours == "09") & (30 <= minutes < 60)]
 
+# a = ts.keys()
+
+#filter out values before 10am because they mess up the data
+# index_value = data.index
+# hours = int(index_value.str[11:13])
+# minutes = int(index_value.str[14:16])
+# data.ix[(hours == "09") & (30 <= minutes < 60)]
+
+numpyStock1 = tuplea[0].as_matrix()
+numpyStock2 = tuplea[1].as_matrix()
+
+slopes1 = np.array(convert_to_slopes(numpyStock1))
+slopes2 = np.array(convert_to_slopes(numpyStock2))
+
+cost = calc_cost(slopes1, slopes2)
+#gets the first 5 vals
+new_cost = calc_cost(slopes1, slopes2)
 #gets descriptive stats
 cost = 999999999999
 best = None
-for info in data:
-    if calc_cost(stock2, info) < cost:
-        best = info
-        cost = calc_cost(stock2, info)
+
+length = len(slopes2)
+for shift in range(length -1):
+    if calc_cost(slopes2[shift:length-1], slopes1[0:length -1 - shift]) < cost:
+        best = shift
+        cost = calc_cost(slopes2[shift:length-1], slopes1[0:length -1]) < cost
+
+print(shift)
 
 
 
